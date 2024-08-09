b8ab29b87ff5e3dce45b02bf7686411ff2c80c88
Loic Tetrel <loic.tetrel@kitware.com>
2024-08-09 17:14:02 +0200

    [feat] improving opencv build script for jetson

---
+++ scripts/install_opencv4.8.0_Jetson.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA Corporation and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA Corporation is strictly prohibited.
+#
+# slightly modified from https://forums.developer.nvidia.com/t/compiling-opencv-on-jetpack-5/219668/5
+
+version="4.8.0"
+folder="opencv-4.8.0-build"
+
+for (( ; ; ))
+do
+    echo "Do you want to remove the default OpenCV (yes/no)?"
+    read rm_old
+
+    if [ "$rm_old" = "yes" ]; then
+        echo "** Remove other OpenCV first"
+        sudo apt -y purge *libopencv*
+	break
+    elif [ "$rm_old" = "no" ]; then
+	break
+    fi
+done
+
+
+echo "------------------------------------"
+echo "** Install requirement (1/4)"
+echo "------------------------------------"
+sudo apt-get update
+sudo apt-get install -y build-essential libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
+sudo apt-get install -y curl
+sudo python3.8 -m pip install "numpy<2"
+
+
+echo "------------------------------------"
+echo "** Download opencv "${version}" (2/4)"
+echo "------------------------------------"
+mkdir -p $folder
+cd ${folder}
+curl -L https://github.com/opencv/opencv/archive/${version}.zip -o opencv-${version}.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/${version}.zip -o opencv_contrib-${version}.zip
+unzip opencv-${version}.zip
+unzip opencv_contrib-${version}.zip
+rm opencv-${version}.zip opencv_contrib-${version}.zip
+cd opencv-${version}/
+
+
+echo "------------------------------------"
+echo "** Build opencv "${version}" (3/4)"
+echo "------------------------------------"
+mkdir release
+cd release/
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
+-D WITH_CUDA=ON \
+-D WITH_CUDNN=ON \
+-D WITH_TBB=ON \
+-D ENABLE_FAST_MATH=1 \
+-D CUDA_FAST_MATH=1 \
+-D WITH_CUBLAS=1 \
+-D BUILD_opencv_cudacodec=ON \
+-D OPENCV_DNN_CUDA=ON \
+-D WITH_QT=OFF \
+-D WITH_OPENGL=ON \
+-D BUILD_opencv_apps=OFF \
+-D BUILD_opencv_python2=OFF \
+-D OPENCV_PC_FILE_NAME=opencv.pc \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D INSTALL_C_EXAMPLES=OFF \
+-D CUDA_ARCH_BIN="7.2,8.7" \
+-D CUDA_ARCH_PTX="" \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules \
+-D WITH_GSTREAMER=ON \
+-D WITH_LIBV4L=ON \
+-D BUILD_opencv_python3=ON \
+-D BUILD_TESTS=OFF \
+-D BUILD_PERF_TESTS=OFF \
+-D BUILD_EXAMPLES=OFF \
+-D WITH_FFMPEG=ON \
+..
+
+make -j$(nproc)
+
+
+echo "------------------------------------"
+echo "** Install opencv "${version}" (4/4)"
+echo "------------------------------------"
+sudo make install
+
+
+echo "** Install opencv "${version}" successfully"
+echo "** Bye :)"
